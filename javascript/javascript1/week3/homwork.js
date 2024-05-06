399f65bc351e568cee10507f19a2287011eaa75a
AliRezaHassanloo2731 <reza1632731@gmail.com>
2024-05-06 15:13:01 +0330

    add When will we be there??

--- javascript/javascript1/week3/homwork.js
+++ javascript/javascript1/week3/homwork.js
@@ -19,3 +19,31 @@ console.log(names.splice(idxOfNameToRemove, 1));
 
 //original array
 console.log(names);
+
+//todo :second exercise
+//When will we be there??
+//data
+const travelInformation = {
+  speed: 50,
+  destinationDistance: 432,
+};
+//logic
+const arrivalTime = function (speed, distance) {
+  const time = (distance / speed) * 60;
+  const minutes = Math.floor(time % 60);
+  const hours = Math.floor(time / 60);
+  let message;
+  if (time >= 1) {
+    message = `${hours} hours and ${minutes} minutes`;
+  } else {
+    message = `${time} minutes`;
+  }
+  return message;
+};
+//rendering
+
+const travelTime = arrivalTime(
+  travelInformation.speed,
+  travelInformation.destinationDistance
+);
+console.log(travelTime); // 8 hours and 38 minutes
