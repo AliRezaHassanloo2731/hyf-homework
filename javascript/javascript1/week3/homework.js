c54c530b80210f7419e860104275a174df8a8188
AliRezaHassanloo2731 <reza1632731@gmail.com>
2024-05-08 16:45:19 +0330

    add Smart phone usage app

--- javascript/javascript1/week3/homework.js
+++ javascript/javascript1/week3/homework.js
@@ -116,3 +116,92 @@ console.log(lifeDurationHourly(80));
 // The Wire took 0.008% of my life
 
 // In total that is 0.3% of my life
+//--------------------------------------------------------------
+//CactusIO-interactive (Smart phone usage app)
+//*DATA
+
+const activities = [];
+// let amount = 0;
+let numActivitiesForToday = 0;
+const limit = 100;
+const option = {
+  year: "numeric",
+  month: "narrow",
+  day: "2-digit",
+};
+const option2 = {
+  year: "numeric",
+  month: "short",
+  day: "2-digit",
+};
+const dateEvent = new Date("2024,9,12").toLocaleString("en-US", option2);
+const today = new Date().toLocaleDateString("fa-IR", option);
+
+//*LOGIC
+function addActivity(date, activity, duration) {
+  const activitySchedual = {
+    date: date,
+    activity: activity,
+    duration: duration,
+  };
+  activities.push(activitySchedual);
+}
+addActivity(dateEvent, "Youtube", 30);
+addActivity(today, "chess", 50);
+addActivity(today, "instagram", 25);
+addActivity(today, "telegram", 15);
+console.log("activities=", activities);
+
+function limitation(amount) {
+  if (amount > limit) {
+    console.log("You have reached your limit, no more smartphoning for you!");
+  } else {
+    console.log(
+      ` "You have added ${numActivitiesForToday} activities for today. They amount to ${amount} min. of usage"`
+    );
+  }
+}
+
+function calculateLimit() {
+  let amount = 0;
+  // let numActivitiesForToday = 0;
+  for (let i = 0; i <= activities.length - 1; i++) {
+    if (activities[i].date === today) {
+      amount += activities[i].duration;
+      numActivitiesForToday++;
+    } else {
+      console.log(`${activities[i].activity} is for future`);
+    }
+  }
+
+  limitation(amount);
+}
+
+function findMaxActivityDuration() {
+  const durationArray = [];
+  activities.map((el) => {
+    durationArray.push(el.duration);
+  });
+  const maxDuration = durationArray.reduce((acc, cur) => {
+    return Math.max(acc, cur);
+  }, durationArray[0]);
+  //print max elemnt's activity
+  activities.find((el) => {
+    if (el.duration === maxDuration) {
+      console.log(
+        `${el.activity} is the most time consuming acitivity on phone `
+      );
+    }
+  });
+}
+
+function showStatus() {
+  if (activities.length === 0) {
+    //activity is empty
+    console.log("Add some activities before calling showStatus");
+  } else {
+    calculateLimit();
+  }
+  findMaxActivityDuration();
+}
+showStatus(activities); //  "You have added 2 activities. They amount to 90 min. of usage"
